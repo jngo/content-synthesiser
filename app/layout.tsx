7b524749b79759e8f3dac6c5a99331aba644f2d2
John Ngo <jngo@users.noreply.github.com>
2025-03-31 23:55:48 +0200

    Enabled Vercel analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import 'reactflow/dist/base.css'
 import "./globals.css"
 import { Inter } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -17,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
